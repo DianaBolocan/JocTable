1b3149dfcb1187ecdb2a7b8dde3abdcb38a255b7
DianaBolocan <DianaBolocan@users.noreply.github.com>
2016-12-22 16:45:52 +0200

    Add files via upload

---
+++ Table v.0.0.2/proiect.h
@@ -0,0 +1,24 @@
+struct scor
+{
+    short partide;
+    short linii;
+};
+
+struct zar{
+	short zar1, zar2;
+};
+
+struct player
+{
+	char nume[10];
+	scor scor;
+	zar zaruri;
+	char piesa;
+	short eliminate;
+};
+
+struct stiva
+{
+	char inf;
+	stiva *urm;
+}stiva *prim;
